48bb0872670031ac1f7c15cc951c3049bc7eee87
Andrew Gartland <agartlan@fhcrc.org>
2020-05-27 15:17:29 -0700

    Bumping version for pypi

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ opts = dict(name='hierdiff',
             license='MIT',
             author='Andrew Fiore-Gartland',
             author_email='agartlan@fredhutch.org',
-            version='0.1.1',
+            version='0.1.2',
             packages=PACKAGES
            )
 
